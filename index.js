59672c8833898e01ea7de752a4145c3190d795d1
matthew.pikar <matt.piekarczyk@cortixsystems.com>
2016-01-06 13:17:20 -0500

    removed semicolons

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /**
  * Created by mattpiekarczyk on 11/4/15.
  */
-"use strict";
+"use strict"
 
-var _ = require('lodash');
-var options = {};
+var _ = require('lodash')
+var options = {}
 
 for(var i= 2, len=process.argv.length; i<len; i++){
-    var argument = process.argv[i].split(':');
-    options[argument[0]] = argument[1];
+    var argument = process.argv[i].split(':')
+    options[argument[0]] = argument[1]
 }
 
 _.extend(options, {
@@ -20,7 +20,7 @@ _.extend(options, {
         movementId: 'string$',
         description: 'string$'
     }
-});
+})
 
 require('seneca')()
     .use('redis-transport')
@@ -30,4 +30,3 @@ require('seneca')()
     .listen({type:'redis', pin:'role:actions,cmd:add'})
     .listen({type:'redis', pin:'role:actions,cmd:modify'})
     .listen({type:'redis', pin:'role:actions,cmd:delete'})
-;
